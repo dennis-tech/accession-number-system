760172821a17db7272b35ab62f07884540e66610
Dennis Stanely Irungu <hedenniskibe@gmail.com>
2022-08-17 07:43:03 +0300

    modified main.php file

--- main.php
+++ main.php
@@ -1,16 +1,12 @@
 <?php 
 
 session_start();
-
 require_once "config.php";
-
-include 'readJson.php';
-
 
 if (!isset($_SESSION['username'])) {
     header("Location: index.php");
 }
-
+// open connection
  date_default_timezone_set('Africa/Nairobi');
     if(isset($_GET['arruy'])){
     $data = explode (",", $_GET['arruy']);
@@ -41,7 +37,6 @@ if (!isset($_SESSION['username'])) {
     // $dennisrowcount = mysqli_num_rows($dennisresult);
     // $dennisrowcount = $dennisrowcount - 1;
     
-
 
 
 ?>
@@ -88,7 +83,6 @@ if (!isset($_SESSION['username'])) {
             <button type="submit" id="save-btn">SAVE</button>
             <p id="number"><br></p>
             
-
     <button id = "logout" > <a href="logout.php">Log out</a> </button> 
       
     
@@ -138,55 +132,6 @@ if (!isset($_SESSION['username'])) {
           
         });
       </script>
-
-        <button id = "logout" > <a href="logout.php">Log out</a> </button> 
-      
-    
-    
-      <script>
-            let dateOptions = { timeZone: 'UTC', month: 'long', day: 'numeric', year: 'numeric' };
-            let leo = new Date();
-            let today = leo.toLocaleDateString('en-US', dateOptions);
-            // var javar =                              //<'?=$lastnumber?'>;
-            console.log(javari);
-            let saveBtn = document.getElementById("save-btn");
-            let totalCalc = {
-            calcNumber: 100,
-            currentNumber: javar
-            };
-
-            let generatedRepo = "";
-            let numberReport = document.getElementById("number");
-            let total = totalCalc.currentNumber  + totalCalc.calcNumber; 
-            function generateNo () {
-                let selectedValue = document.getElementById("person").value; 
-                
-                    if (selectedValue === "--Select Person--") {
-                alert ("You have not selected any person to assign to!");
-
-                } else {      
-                generatedRepo += `<li> <span class = "selectedperson"> ${selectedValue}</span> has been assigned numbers from 
-                <span class = "currno">${totalCalc.currentNumber.toLocaleString()}</span> to <span class = "total">
-                ${total.toLocaleString()}</span> on <span class = "today">${today}</span> </li>`;
-                numberReport.innerHTML = generatedRepo;
-                }
-            }
-
-            saveBtn.addEventListener("click",function () {
-                let selectedValue = document.getElementById("person").value;
-                let arruy = [selectedValue, totalCalc.currentNumber, total];
-                console.log(arruy);
-                if (selectedValue === "--Select Person--") {
-                    alert ("You have not selected any person to assign to!");
-
-                } else {  
-                    let src1 = "main.php?arruy=" + arruy[0] + "," + arruy[1]  + "," + arruy[2];
-                    window.location.href = src1;
-                }
-            
-            })
-        </script>
-
               
     </body>
 </html>
